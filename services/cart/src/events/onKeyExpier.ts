1918fe389081d53de17912fba41b70c68a17abbb
arifnextdev <arif171042@gmail.com>
2025-04-17 02:45:58 +0600

    cart service done

--- services/cart/src/events/onKeyExpier.ts
+++ services/cart/src/events/onKeyExpier.ts
@@ -1,4 +1,5 @@
 import { REDIS_HOST, REDIS_PORT } from "@/config";
+import ClearCart from "@/services";
 import { Redis } from "ioredis";
 
 const redis = new Redis({
@@ -12,6 +13,11 @@ redis.config("SET", "notify-keyspace-events", "Ex");
 redis.subscribe(CHANNEL_KEY);
 redis.on("message", async (channel, message) => {
   if (channel === CHANNEL_KEY) {
+    const cartKey = message.split(":").pop();
+
+    if (!cartKey) return;
+    ClearCart(cartKey);
+
     console.log("Key expired", message);
   }
 });
